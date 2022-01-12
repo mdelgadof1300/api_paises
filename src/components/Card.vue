f9131fedb77369b704b6c1ddca4125413d5b677e
Miguel Mauricio Delgado Fernández <73376209+mdelgadof1300@users.noreply.github.com>
2022-01-12 16:02:58 -0600

    API de Países con Vue 3
    npm i

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card mb-2">
+    <div class="card-body">
+      <h5 class="text-center">{{ pais.name.official }}</h5>
+      <p class="text-center">
+        <img
+          :src="pais.flags.svg"
+          :alt="`bandera-${pais.name.official}`"
+          class="img-fluid w-50"
+        />
+      </p>
+      <p class="card-text">
+        <span class="badge badge-dark d-block mb-1"
+          >Nombre nativo: {{ pais.nativeName }}</span
+        >
+        <span class="badge badge-info p-3 d-block mb-1"
+          >Población: {{ numeroFormato(pais.population) }}</span
+        >
+        <span class="badge badge-dark d-block mb-1"
+          >Capital: {{ pais.capital }}</span
+        >
+        <span class="badge badge-dark d-block mb-1"
+          >Región: {{ pais.region }}</span
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pais"],
+  setup() {
+    const numeroFormato = (num) => {
+      return new Intl.NumberFormat("de-DE").format(num);
+    };
+
+    return { numeroFormato };
+  },
+};
+</script>
